efdbe86fc18b9c937c137dfbabd70742aec7d0a1
Mam <mamchristian310@gmail.com>
2025-12-04 23:07:29 +0100

    File changed to a pyramid pattern function.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World ! My name is Mam Christian")
+def pyramid(n):
+    """Generate a pyramid pattern of asterisks with n levels."""
+    for i in range(1, n + 1):
+        # Print leading spaces
+        print(' ' * (n - i), end='')
+        # Print asterisks
+        print('* ' * i)
+        
+# Example usage
+pyramid(10)
